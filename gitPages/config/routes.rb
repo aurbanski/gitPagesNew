4af4db5f5941e71b0bd23f042c61430eb42ee5e1
aurbanski <aurbanski27@gmail.com>
2017-04-22 17:43:19 -0600

    Changed home

--- gitPages/config/routes.rb
+++ gitPages/config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+
   get "/auth/:provider/callback", to: "sessions#create"
   get 'auth/failure', to: redirect('/')
   delete 'signout', to: 'sessions#destroy', as: 'signout'
   root to: 'sessions#new'
 
   get 'sessions/new'
-
   get 'sessions/create'
-
   get 'sessions/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
