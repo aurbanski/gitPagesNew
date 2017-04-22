b8ec753fbdc8a5ac6c5e77f413ad7ceb28add4f1
aurbanski <aurbanski27@gmail.com>
2017-04-22 16:16:07 -0600

    Set up github

--- gitPages/config/routes.rb
+++ gitPages/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get "/auth/:provider/callback", to: "sessions#create"
+  get 'auth/failure', to: redirect('/')
+  delete 'signout', to: 'sessions#destroy', as: 'signout'
+  root to: 'sessions#new'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
